7bb70d5b5949eb8202df82c9239ddd617e9ff11f
JasonKiptoo <kiptoojason@gmail.com>
2022-11-09 19:05:23 +0300

    updated the card

--- src/components/featuredEvents.js
+++ src/components/featuredEvents.js
@@ -1,50 +1,15 @@
 import React from "react";
+import EventCard from "./EventCard";
 
 const FeaturedEvents = ({ events }) => {
+  const eventList = events.map((event) => (
+    <EventCard key={event.id} event={event} />
+  ));
+  // console.log(events.name)
+
   return (
     <div className="ui four column grid">
-      {events.map((event) => (
-        <div className="ui card">
-          <div class="container mx-auto mt-4">
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card-event">
-                  <img
-                    src="https://miro.medium.com/max/720/1*_0u4XzW7F-lKTMOv1ldkNg.jpeg"
-                    class="card-img-top"
-                    alt="..."
-                  />
-                  <div class="card-body">
-                    {/* <h5 class="card-title">{event.name}</h5> */}
-                    <h5 class="card-date text-muted">5th Nov 2022</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{event.name}</h6>
-                    <p class="card-text">
-                      GDG DevFest Nairobi is a 100% community organized
-                      conference with 2000+ amazing engineers (embarking on a
-                      nomadic
-                      {/* journey from all corners of the continent){damage} */}
-                    </p>
-
-                    <span>
-                      <i className="icon shield" />
-                      {/* {armor} */}
-                    </span>
-                    <a href="#" class="btn mr-2">
-                      <i class="fas fa-link"></i> Check Event
-                    </a>
-                    <a href="#" class="btn ">
-                      <i class="fab fa-github"></i> Book Now
-                    </a>
-                    <a href="#" class="btn ">
-                      <i class="fab fa-github"></i> 9 Slots
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+      <div className="row">{eventList}</div>
     </div>
   );
 };
