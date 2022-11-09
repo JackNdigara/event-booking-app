c9b4bffbb27c6ef5dbc45bb83cfe4568b433086a
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-09 10:22:51 +0300

    Added navbar an

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FeaturedAttendees from "./components/attendees/FeaturedAttendees";
+import FormLogin from "./components/Login/FormLogin";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => {
+        const newA = data.sort(function (a, b) {
+          var dateA = new Date(a.date),
+            dateB = new Date(b.date);
+          return dateA - dateB;
+        });
+        setData(newA);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/form"
+            element={<FormLogin data={data} setData={setData} />}
+          />
+
+          <Route
+            path="/featured"
+            element={<FeaturedAttendees data={setData} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/attendees/FeaturedAttendees.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeaturedAttendees = () => {
+  return (
+    <div>FeaturedAttendees</div>
+  )
+}
+
+export default FeaturedAttendees
