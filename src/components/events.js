e10fedf3f40d57d6e248c184eabbe237b838ec0c
jasonkiptoo <45195231+jasonkiptoo@users.noreply.github.com>
2022-11-11 08:34:31 +0300

    Update events.js
    Changed navbar/event color card to dark

--- src/components/events.js
+++ src/components/events.js
@@ -6,7 +6,7 @@ const Event = () => {
       {/* <!-- Header--> */}
       <header class="py-5">
         <div class="container px-lg-5">
-          <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
+          <div class="p-4 p-lg-5 bg-dark rounded-3 text-center">
             <div class="m-4 m-lg-5">
               <h1 class="display-5 fw-bold">Welcome !!</h1>
               <p class="fs-4">
